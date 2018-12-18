a8181ceb58afc69b716666bb6d8db876b7ecd053
myasinK <yasinul@gmail.com>
2018-12-17 17:47:23 -0800

    Save pcpa events to json

--- event-scraper/pcpa_helpers.py
+++ event-scraper/pcpa_helpers.py
@@ -24,13 +24,14 @@ def print_days( start_date, num_days ):
     return dates_array, dates_str
 
 
-def read_info_from_page(url, payload):    
+def read_info_from_page(url, payload, events_json):    
     r = requests.get(url, params = payload)
     soup = BeautifulSoup(r.content)
     data = soup.find_all("div", {"class": "views-row"})
     check_next_page = False
     append_line = ""
     pcpa_link = "https://www.portland5.com"
+    
     for each_entry in data:
         if each_entry.h3 is None:
             pass
@@ -50,8 +51,13 @@ def read_info_from_page(url, payload):
                 event_dates = start_date + '/19'
             append_line += event + '\n' + event_venue + '\n' + event_page + '\n' + event_dates + '\n\n'
             print( append_line )    
-            event_details = ""
-    return check_next_page, append_line
+            events_json['pcpa'].append( { 
+               'event' : event,
+               'event-venue' : event_venue,
+               'event-page' : event_page,
+               'event-dates' : event_dates
+                } )
+    return check_next_page, append_line, events_json
 
 
 def scrape_pcpa( file_path ):
@@ -59,11 +65,15 @@ def scrape_pcpa( file_path ):
     page_num = 1    
     fopen = open( file_path, 'a', encoding='utf-8' )
     check_next_page = True
+    events_json = {}
+    events_json['pcpa'] = []
     while ( check_next_page ):
         payload = { "page" : str(page_num) }
-        check_next_page, line = read_info_from_page(url, payload)
+        check_next_page, line, events_json = read_info_from_page( url, payload, events_json )        
         write = fopen.write(line) if len( line ) > 0 else -1
         page_num += 1 if check_next_page else 0 
-    print('\n\n' + "page number: " + str( page_num ))
+    with open('C:\\xampp\\htdocs\\events-json.txt', 'w') as json_file:
+        json.dump( events_json, json_file )
+    print( '\n\n' + "page number: " + str( page_num ) )
     fopen.close()
     return
